754e722dcc3b7d2db1f9a417192beaaebe481e0b
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-09-22 16:05:08 +0100

    Mock methods

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,15 @@
 from click.testing import CliRunner
 from rule_of_three import __main__
-from mock import patch
+from unittest.mock import patch
 
 def test_main():
-    runner = CliRunner()
-    result = runner.invoke(__main__.main, ['1', '2', '3'])
-    assert result.exit_code == 0
-    assert result.output == '6.0\n'
+    with patch("rule_of_three.__main__.rule_of_three") as rule_of_three_mocked:
+
+        rule_of_three_mocked.return_value = 0
+
+        runner = CliRunner()
+        result = runner.invoke(__main__.main, ['1', '2', '3'])
+
+        rule_of_three_mocked.assert_called_once_with(1, 2, 3)
+        assert result.exit_code == 0
+        assert result.output == '0\n' 
